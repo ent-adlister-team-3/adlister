12d1b89ea81c06bb971f6a5b8ea2ffa2de25da24
Rick Morales <rick.morales7693@gmail.com>
2023-09-08 09:46:55 -0500

    fixed SeederSQL

--- SeederSQL.sql
+++ SeederSQL.sql
@@ -1,21 +1,19 @@
 USE puff_db;
 
-DROP TABLE IF EXISTS ads;
-DROP TABLE IF EXISTS users;
+INSERT INTO users (username, email, password, phone_number)
+VALUES ('John', 'john@email.com', 'password', '1234567890'),
+       ('Jane', 'jane@email.com', 'password', '1234567890'),
+       ('Bob', 'bob@email.com', 'password', '1234567890');
 
-INSERT INTO ads (user_id, title, description, price) VALUES
-    (1, 'Clothes', 'Clothes', 100),
-    (1, 'Shirt', 'Shirt', 25),
-    (1, 'Pants', 'Pants', 25),
-    (2, 'Shoes', 'Shoes', 50),
-    (2, 'Hat', 'Hat', 10),
-    (2, 'Gloves', 'Gloves', 15),
-    (3, 'Socks', 'Socks', 5),
-    (3, 'Jacket', 'Jacket', 75),
-    (3, 'Scarf', 'Scarf', 20),
-    (3, 'Belt', 'Belt', 10);
+INSERT INTO ads (user_id, title, description, price)
+VALUES (1, 'Clothes', 'Clothes', 100),
+       (1, 'Shirt', 'Shirt', 25),
+       (1, 'Pants', 'Pants', 25),
+       (2, 'Shoes', 'Shoes', 50),
+       (2, 'Hat', 'Hat', 10),
+       (2, 'Gloves', 'Gloves', 15),
+       (3, 'Socks', 'Socks', 5),
+       (3, 'Jacket', 'Jacket', 75),
+       (3, 'Scarf', 'Scarf', 20),
+       (3, 'Belt', 'Belt', 10);
 
-INSERT INTO users (username, email, password, phone_number) VALUES
-    ('John', 'john@email.com', 'password', '1234567890'),
-    ('Jane', 'jane@email.com', 'password', '1234567890'),
-    ('Bob', 'bob@email.com', 'password', '1234567890');
